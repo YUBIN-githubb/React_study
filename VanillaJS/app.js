5c57330e6538d85c6d55f6e381e31738dbbc6909
YUBIN-githubb <yubin5744@gmail.com>
2026-08-05 21:14:39 +0900

    array

--- VanillaJS/app.js
+++ VanillaJS/app.js
@@ -10,3 +10,11 @@ const c = null;
 let d;
 console.log(c);
 console.log(d);
+
+// Getting
+const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+console.log(daysOfWeek[0]);
+
+// Pushing
+daysOfWeek.push('Sun');
+console.log(daysOfWeek);
