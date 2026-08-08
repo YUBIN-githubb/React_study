38ddea4763e9ce36cde257577e225b51b4dc069f
YUBIN-githubb <yubin5744@gmail.com>
2026-08-08 15:01:30 +0900

    condition

--- VanillaJS/app.js
+++ VanillaJS/app.js
@@ -43,3 +43,22 @@ function sayHello(name) {
 
 sayHello('Yubin');
 player.sayHi();
+
+
+const number = "10"
+console.log(typeof parseInt(number));
+
+
+const age = 90
+
+if (isNaN(age) || age < 0) {
+    console.log('Please write a real positive number');
+} else if (age < 18) {
+    console.log('You are too young');
+} else if (age >= 18 && age <= 50) {
+    console.log('You can drink');
+} else if (age > 50 && age <= 80) {
+    console.log('You should exercise');
+} else {
+    console.log('You can do whatever you want');
+}
